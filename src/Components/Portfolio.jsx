ceeafbbe306c4ce1e03f0080a317e337ec5698c3
Cloudmavin03 <Cloudmavin03@users.noreply.github.com>
2023-12-04 07:24:35 +0100

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Smart Home Threat Model with the Microsoft Threat Modeling tool",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Designed a threat model using the MTM tool to identify potential threats to a smart home device. This was a part of a Microsoft workshop on threat modeling.",
+    url: "",
   },
   {
-    title: "Web Development for Beginners",
+    title: "",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "",
+    url: "",
   },
   {
-    title: "My Resume Site",
+    title: "",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "",
+    url: "",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "",
+    url: "",
   },
 ];
 
